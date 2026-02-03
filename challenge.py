3a2a4197447fe9b9e70f150cbbb60964a5a93ec5
romendezs <ro.mendez005@gmail.com>
2026-02-03 15:47:54 -0600

    add: camel case function

---
+++ challenge.py
@@ -0,0 +1,15 @@
+def to_camel_case(text):
+    
+    splitted = text.split("_")
+    result = []
+    for word in splitted:
+        print(word)
+        
+        if not word[0].isupper() and word != splitted[0]:
+            result.append(word[0].upper() + word[1:])
+        else:
+            result.append(word)  
+    
+    return "".join(result)
+
+print(to_camel_case("hola_Esto_es_una_prueba"))
